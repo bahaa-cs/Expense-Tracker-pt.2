a907afaafeae871f14986a75ed535e866c94b860
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-08 19:35:09 +0200

    Edit: budget.js logic for the mysql database

--- scripts/budget.js
+++ scripts/budget.js
@@ -1,12 +1,17 @@
 let budget= 0
-let transactions =JSON.parse(localStorage.getItem("Transactions")) || []
-
-const totalBudget = () => {transactions.forEach(transaction => {
-    if (transaction.type !== "")
+let transactions;
+axios.post('http://localhost:8080/expense-tracker-server/apis/getTotalBudget.php',
+    {
+        users_id : 1
+    }
+)
+.then((response) =>{
+    transactions = response.data || [];
+    transactions.forEach(transaction => {
         budget = transaction.type=="income" ? budget+Number(transaction.price) : budget-Number(transaction.price)
-    
-    })
+        })
     document.getElementById("budget-total").innerText = `$ ${budget}`
-}
-totalBudget()
+    })
+
+
 
